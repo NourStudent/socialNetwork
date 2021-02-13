24523da48869bfe30fd9f219b8cf5a46f648e69c
User <user@Users-MacBook-Pro.local>
2021-02-13 10:06:32 -0500

    Getting Started

---
+++ socialNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  socialNetwork
+//
+//  Created by User on 2021-02-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
